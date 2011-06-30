7ea527734e7fd4545ec690ef960b3ee6ae945ef0
Diego Andres Sanabria Martin <diegueus9@gmail.com>
2011-06-30 17:57:42 -0500

    Initial code for formtools

---
+++ formtools/templatetags/formtools.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from django import template
+from django.core.urlresolvers import reverse
+from django.forms import Form, ModelForm
+from django.template import Variable
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext as _
+
+register = template.Library()
+
+def split_args(args):
+    #TODO hacer mejor y mover:
+    #los parametros son posicionales
+    # los opt returnan un diccionario
+    tag = ''
+    opts = []
+    kwargs = {}
+    tag = args[0]
+    for arg in args[1:]:
+        if '=' in arg:
+            opt, value = arg.split('=')
+            opt = str(opt)
+            if value[0]==value[-1] and (value[0]=="'" or value[0]=='"'):
+                kwargs[opt] = value[1:-1]
+            elif value=='True':
+                kwargs[opt] = True
+            elif value=='False':
+                kwargs[opt] = False
+            else:
+                kwargs[opt] = value
+        else:
+            opts.append(arg)
+    return tag, opts, kwargs
+
+
+class RenderFormNode(template.Node):
+
+    def __init__(self, form, **kwargs):
+        if not isinstance(form, (Form, ModelForm)):
+            self.form = template.Variable(form)
+        else:
+            self.form = form
+        self.placeholder = kwargs.get('placeholder', False)
+        self.button_name = kwargs.get('button_name', _('Send'))
+        self.action_name = kwargs.get('action_name', None)
+        self.action_url = kwargs.get('action_url', '/')
+        self.template_form = kwargs.get('template_form', 'form/form.html')
+        self.template_field = kwargs.get('template_field', 'form/field.html')
+        if self.action_name:
+            self.action = reverse(self.action_name)
+            self.action_id = self.action_name
+        else:
+            self.action = self.action_url
+            self.action_id = '-'.join(self.action_url.split('/'))
+        self.upload_files = kwargs.get('upload_files', False)
+
+    def render(self, context):
+        try:
+            if isinstance(self.form, Variable):
+                form = self.form.resolve(context)
+            else:
+                form = self.form
+            c = {}
+            c['form'] = form
+            c['placeholder'] = self.placeholder
+            c['button_name'] = self.button_name
+            c['action'] = self.action
+            c['action_id'] = self.action_id
+            if self.upload_files:
+                c['upload'] = mark_safe('enctype="multipart/form-data"')
+            else:
+                c['upload'] = ''
+            fields = ""
+            for field in form:
+                fields += render_to_string(self.template_field, {'field':field}, context_instance=context)
+            c['fields'] = mark_safe(fields)
+            return mark_safe(render_to_string(self.template_form, c, context_instance=context))
+        except template.VariableDoesNotExist:
+            return ''
+
+@register.tag
+def render_form(parser, token):
+    args = token.split_contents()
+    tag, opts, kwargs = split_args(args)
+    return RenderFormNode(*opts,**kwargs)
+
+#@register.tag
+#def render_edit_profile_form(parser, token):
+    #args = token.split_contents()
+    #tag, opts, kwargs = split_args(args)
+    #prefix = kwargs.get('prefix', '')
+    #opts = [EditProfileForm(prefix=prefix)]
+    #kwargs['upload_files'] = True
+    #kwargs['action_name'] = 'edit_profile'
+    #return RenderFormNode(*opts,**kwargs)
+
+#@register.tag
+#def render_signin_profile_form(parser, token):
+    #args = token.split_contents()
+    #tag, opts, kwargs = split_args(args)
+    #prefix = kwargs.get('prefix', '')
+    #opts = [SignInForm(prefix=prefix)]
+    #kwargs['action_name'] = 'signin'
+    #return RenderFormNode(*opts,**kwargs)
+
+#@register.tag
+#def render_login_profile_form(parser, token):
+    #args = token.split_contents()
+    #tag, opts, kwargs = split_args(args)
+    #prefix = kwargs.get('prefix', '')
+    #opts = [LoginForm(prefix=prefix)]
+    #kwargs['action_name'] = 'profile_login'
+    #return RenderFormNode(*opts,**kwargs)
+
+#@register.tag
+#def render_password_forgotten_form(parser, token):
+    #args = token.split_contents()
+    #tag, opts, kwargs = split_args(args)
+    #prefix = kwargs.get('prefix', '')
+    #opts = [PasswordForgottenForm(prefix=prefix)]
+    #kwargs['action_name'] = 'password_forgotten'
+    #return RenderFormNode(*opts,**kwargs)
+
+#@register.tag
+#def render_change_password_form(parser, token):
+    #args = token.split_contents()
+    #tag, opts, kwargs = split_args(args)
+    #prefix = kwargs.get('prefix', '')
+    #opts = [ChangePasswordForm(prefix=prefix)]
+    #kwargs['action_name'] = 'change_password'
+    #return RenderFormNode(*opts,**kwargs)
